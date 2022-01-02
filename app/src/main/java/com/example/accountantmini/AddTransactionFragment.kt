23f053d7e9476830c6ee4f633f0b6e8702126720
Akshat Trivedi <akshattrivedi7901@gmail.com>
2022-01-02 22:50:30 +0530

    Add Transaction and Account List fragment implemented

---
+++ app/src/main/java/com/example/accountantmini/AddTransactionFragment.kt
@@ -0,0 +1,27 @@
+package com.example.accountantmini
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+
+
+class AddTransactionFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        (activity as AppCompatActivity?)!!.supportActionBar!!.show()
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_transaction, container, false)
+    }
+}
