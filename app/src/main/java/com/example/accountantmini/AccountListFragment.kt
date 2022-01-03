261a1719a6e4c3dc5e1d4f7c720c7a63cf969392
Akshat Trivedi <akshattrivedi7901@gmail.com>
2022-01-03 11:20:57 +0530

    fragment_account_list.xml design, Add account Fragment implemented

--- app/src/main/java/com/example/accountantmini/AccountListFragment.kt
+++ app/src/main/java/com/example/accountantmini/AccountListFragment.kt
@@ -1,14 +1,20 @@
 package com.example.accountantmini
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import com.example.accountantmini.databinding.FragmentAccountListBinding
+
 
 
 class AccountListFragment : Fragment() {
+
+    private var _binding: FragmentAccountListBinding? = null
+    private val binding get() = _binding!!
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +26,15 @@ class AccountListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account_list, container, false)
+        _binding = FragmentAccountListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.floatingActionButton.setOnClickListener {
+            val action = AccountListFragmentDirections.actionAccountListFragmentToAddAccountFragment()
+            findNavController().navigate(action)        }
     }
 }
